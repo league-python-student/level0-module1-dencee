e8a5a7122463f8430eba837b37939ca94f70e0a5
root <root@root.com>
2020-12-11 03:26:25 +0000

    Test 2

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -1,3 +1,6 @@
 # Write a Python program that asks the user for the radius 
 # of a circle and displays the area of that circle
 # The formula for the area of a circle is πr^2
+#new code
+if __name__ == '__main__':
+    print( 'hello world!')
